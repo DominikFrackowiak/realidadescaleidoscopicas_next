4ca2e3f213de3983e189cf1186c172ab08924889
DominikFrackowiak <rorschach@op.pl>
2024-07-02 23:11:58 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-
+import { Suspense } from 'react'
 
 import Stages from '../components/organisms/Stages'
 
-export default function Home( ) {
-	
+export default function Home() {
 	return (
 		// <main className='flex min-h-screen flex-col items-center justify-between p-24'>
 		// 	<Stages/>
 		// </main>
 		<main>
-			<Stages />
+			<Suspense>
+				<Stages />
+			</Suspense>
 		</main>
 	)
 }
