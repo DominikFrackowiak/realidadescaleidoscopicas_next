9850e5420c798f7fe59001e2c0bf432eaed7dbdc
Dominik Frackowiak <86988772+DominikFrackowiak@users.noreply.github.com>
2024-08-10 16:48:38 +0200

    update

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -4,30 +4,28 @@ import Stack from '../atoms/Stack'
 import Heading from '../atoms/Heading'
 
 const LeftColumn = () => (
- <Stack classes=''>
-   <Heading headingType={'h3'}
-						className=' text-white  text-2xl text-center'>
-  </Heading>
- </Stack>
+	<Stack classes=''>
+		<Heading headingType={'h3'} className=' text-white  text-2xl text-center'>
+			TEst
+		</Heading>
+	</Stack>
 )
 
-const RightColumn = () => (
- <p>Right</p>
-)
+const RightColumn = () => <p>Right</p>
 
 export default function Footer() {
-  return (
-    <footer className='bg-[#364ec6]'>
-     <section className='flex justify-center' id='footer'>
-     <Wrapper>
-      {/* <SplitScreen left={LeftSide} right={RightSide} classes={'justify-between'}/> */}
-      <SplitScreen
-					Left={LeftColumn}
-					Right={RightColumn}
-					classes={'justify-between'}
-				/>
-     </Wrapper>
-     </section>
-    </footer>
-  )
+	return (
+		<footer className='bg-[#364ec6]'>
+			<section className='flex justify-center' id='footer'>
+				<Wrapper>
+					{/* <SplitScreen left={LeftSide} right={RightSide} classes={'justify-between'}/> */}
+					<SplitScreen
+						Left={LeftColumn}
+						Right={RightColumn}
+						classes={'justify-between'}
+					/>
+				</Wrapper>
+			</section>
+		</footer>
+	)
 }
