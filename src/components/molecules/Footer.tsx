ab5da67ec53f124d69e1b835207b23f09709cb05
Dominik Frackowiak <86988772+DominikFrackowiak@users.noreply.github.com>
2024-08-10 16:42:46 +0200

    update

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -1,9 +1,33 @@
 import Wrapper from '../atoms/Wrapper'
+import SplitScreen from './SplitScreen'
+import Stack from '../atoms/Stack'
+import Heading from '../atoms/Heading'
+
+const LeftColumn = () => (
+ <Stack classes=''>
+   <Heading headingType={'h3'}
+						className=' text-white  text-2xl text-center'>
+  </Heading>
+ </Stack>
+)
+
+const RightColumn = () => (
+ <p>Right</p>
+)
 
 export default function Footer() {
   return (
-    <footer class='bg-[#364ec6]'>
-     <Wrapper><div>This is footer, motherfuckers</div></Wrapper>
+    <footer className='bg-[#364ec6]'>
+     <section className='flex justify-center' id='footer'>
+     <Wrapper>
+      {/* <SplitScreen left={LeftSide} right={RightSide} classes={'justify-between'}/> */}
+      <SplitScreen
+					Left={LeftColumn}
+					Right={RightColumn}
+					classes={'justify-between'}
+				/>
+     </Wrapper>
+     </section>
     </footer>
   )
 }
